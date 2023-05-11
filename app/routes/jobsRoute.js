dc0c8894ff5640beaee3fac983f16234334ea212
Kaysen_Gamiz <kaysengamiz@gmail.com>
2023-05-10 18:00:23 -0600

    [ROUTES] JobsRoute GET JOBS Added.

--- app/routes/jobsRoute.js
+++ app/routes/jobsRoute.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {Jobs, Users} = require('../controllers/dbSchemas.js');
 
-router.get('/', (req, res) => {
+// GET Jobs
 
-    res.send("Hola, get test.")
+router.get('/', async (req, res) => {
+
+    const jobs = await Jobs.find({});
+    console.log(jobs);
+    
+    res.status(200).send(jobs);
 
 });
 
